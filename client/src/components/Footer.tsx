c101ac9395dbef9897bc35903dca3dd9cd1368b9
Kevin Fremon <kevin@lightbulbmoment.com>
2025-10-09 10:10:28 -0500

    Remove Tips & Tricks link from footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -27,12 +27,6 @@ export default function Footer() {
               >
                 Support
               </button>
-              <Link
-                to="/tips"
-                className="text-sm text-muted-foreground hover:text-foreground transition-colors"
-              >
-                Tips & Tricks
-              </Link>
             </div>
           </div>
         </div>
